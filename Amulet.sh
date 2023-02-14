ccc98eb814aa0e2aa3f3933a18ff5ac5ec69f0a5
Dragonmasterx87 <fahdqadir@gmail.com>
2023-02-13 18:39:44 -0600

    Create Amulet.sh

---
+++ Amulet.sh
@@ -0,0 +1,28 @@
+Path to the bam file (e.g., possorted.bam from CellRanger)
+D:\1. Sex based Study raw data\Cellranger_raw data\snATACseq\1_220628 Fahd_snATAC1_HP-20228-01\possorted_bam.bam
+
+Path to a barcode to cell_id map in CSV format (e.g., singlecell.csv from CellRanger)
+D:\1. Sex based Study raw data\Cellranger_raw data\snATACseq\1_220628 Fahd_snATAC1_HP-20228-01\singlecell.csv
+
+Path to the list of chromosomes to use for the analysis (e.g., human_chromosomes_noxy.txt)
+C:\Users\mqadir\Python\Amulet\human_autosomes.txt
+
+Path to known repetitive elements. (e.g., restrictionlist_repeats_segdups_rmsk_hg19.bed depending on genome)
+D:\1. Sex based Study raw data\Cellranger_raw data\snATACseq\1_220628 Fahd_snATAC1_HP-20228-01\peaks.bed
+
+Path to an existing output directory to write output files.
+D:\3. Coding Scripts and Data\Sex Based study\ATAC_Amulet
+
+Path of this script's directory.
+C:\Users\mqadir\Python\Amulet\AMULET.sh
+
+
+# For 1_220628 Fahd_snATAC1_HP-20228-01
+./
+bash AMULET.sh 
+D:/1.SexbasedStudyrawdata/Cellranger_raw_data/snATACseq/1_220628_Fahd_snATAC1_HP-20228-01/fragments.tsv.gz 
+D:/1.SexbasedStudyrawdata/Cellranger_raw_data/snATACseq/1_220628_Fahd_snATAC1_HP-20228-01/singlecell.csv 
+C:/Users/mqadir/Python/Amulet/human_autosomes.txt 
+C:/Users/mqadir/Python/Amulet/RepeatFilterFiles/blacklist_repeats_segdups_rmsk_hg38.bed
+D:/3.CodingScriptsandData/SexBasedstudy/ATAC_Amulet/ 
+C:/Users/mqadir/Python/Amulet/AMULET.sh
